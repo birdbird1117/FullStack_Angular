f9a550ed85bc59859d6ba6b69fa349fcffa51fbf
birdbird1117 <dapeng.han.1989@gmail.com>
2017-09-16 00:52:42 +0200

    week4, task1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,6 +25,7 @@ dish: Dish;
 promotion: Promotion;
 leader: Leader;
 dishErrMess: string;
+promotionErrMess: string;
 
 constructor(private dishservice: DishService,
   private promotionservice: PromotionService,
@@ -35,7 +36,9 @@ ngOnInit() {
   this.dishservice.getFeaturedDish()
   .subscribe(dish=>this.dish = dish, 
   errmess => this.dishErrMess = <any>errmess);
-  this.promotionservice.getFeaturedPromotion().subscribe(promotion=>this.promotion= promotion);
+  this.promotionservice.getFeaturedPromotion()
+  .subscribe(promotion=>this.promotion= promotion,
+  errmess => this.promotionErrMess = <any>errmess	);
   this.leaderservice.getFeaturedLeader().subscribe(leader=>this.leader=leader);
 }
 
